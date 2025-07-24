63ab6f94b75f7edc4475fb84a007aac332c3f32b
CHETAN BANAIT <121280853+chetanbanait@users.noreply.github.com>
2025-07-24 19:24:06 +0530

    Update testing.py

--- testing.py
+++ testing.py
@@ -7,7 +7,6 @@ cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-
 cam = cv2.VideoCapture(0)
 while True:
     ret, im = cam.read()
